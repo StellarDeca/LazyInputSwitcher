3a58532181e5045946ddca6ed43711c62ba2c19a
StellarDeca <shiwanshuixing@gmail.com>
2025-11-28 14:24:41 +0800

    Remove unnecessary "()"

--- src/static/TreeSitterQuery/python.scm
+++ src/static/TreeSitterQuery/python.scm
@@ -1,5 +1,5 @@
 ; python 注释匹配规则
 (comment) @comment
-(module . (expression_statement (string)) @comment)
-(function_definition (string) @comment)
-(class_definition (string) @comment)
+module . (expression_statement (string)) @comment
+function_definition (string) @comment
+class_definition (string) @comment
