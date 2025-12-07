ec25a9c0fbfd168e6674ef1260950719b04780f8
StellarDeca <shiwanshuixing@gmail.com>
2025-12-07 16:00:04 +0800

    Fix windows switcher mistake

--- src/switch/mod.rs
+++ src/switch/mod.rs
@@ -39,17 +39,17 @@ impl Switcher {
 
     pub(super) fn query(&self) -> Result<InputMethodMode, Box<dyn Error>> {
         #[cfg(target_os = "windows")]
-        self.windows_controller.get_mode()
+        return Ok::<InputMethodMode, Box<dyn Error>>(self.windows_controller.get_mode());
 
         #[cfg(target_os = "linux")]
         self.linux_controller.query()
     }
 
     pub(super) fn switch(&self, target_mode: InputMethodMode) -> Result<bool, Box<dyn Error>> {
-        let mode = self.query();
-        #[cfg(target_os = "windows")]
+        let mode = self.query()?;
         if target_mode != mode {
-            return self.windows_controller.switch_mode(target_mode)
+            #[cfg(target_os = "windows")]
+            return Ok::<bool, Box<dyn Error>>(self.windows_controller.switch_mode(target_mode));
 
             #[cfg(target_os = "linux")]
             return self.linux_controller.switch_mode(mode);
