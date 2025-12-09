8bb1b71ea4b1bda1fc7d147ccbec5a7823cccc3b
StellarDeca <shiwanshuixing@gmail.com>
2025-12-09 21:06:49 +0800

    Update request struct

--- src/rpc/request.rs
+++ src/rpc/request.rs
@@ -3,11 +3,27 @@
 //! {
 //!     cid: u16, // 客户端ID，用于标识客户端身份 值为0时自动分配cid
 //!
-//!     // 命令类型, 当类型为非Command模式时，参数应为0或空串（params将会被忽略）
 //!     // Exit 时服务端将会结束自身的运行，服务端一段时间无客户端连接也会自动退出
-//!     // Command 时 将会执行语法分析 与输入法自动切换
-//!     command: Exit, Command,
+//!     // Switch 时 将会执行语法分析 与输入法自动切换
+//!     // Analyze 时 仅执行 语法分析
+//!     // MethodOnly 时 仅执行输入法切换
+//!     command: Exit, Switcher, Analyze, MethodOnly
 //!
+//!     /// 按照命令类型区分 Analyze 参数
+//!     params: {
+//!         code: String,  // 原始代码
+//!
+//!         // 代码类型,注意首字母大写
+//!         // 名称应与 crate::core::SupportLanguage 枚举中保持一致
+//!         language: String,
+//!     },
+//!
+//!     /// MethodOnly 参数
+//!     params: {
+//!         mode: Native / English,  // 目标输入法, 首字母大写
+//!     },
+//!
+//!     /// Switch 参数
 //!     params: {
 //!         code: String,  // 原始代码
 //!
@@ -15,27 +31,71 @@
 //!         // 名称应与 crate::core::SupportLanguage 枚举中保持一致
 //!         language: String,
 //!
+//!         // 光标位置 UTF-16 字符位置, 0基
 //!         cursor: {
-//!             row: u16,  // 光标行位置 0基
-//!             col: u16  // 光标列位置 0基
-//!         },
+//!             row: usize,
+//!             column: usize
+//!     },
+//!
+//!     /// Exit 参数
+//!     params: {
+//!         // 无参数, 空的 一对花括号
 //!     },
 //! }
 //! ```
 
+use crate::core::Cursor;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) enum CommandMode {
-    Command,
+    Analyze,
+    MethodOnly,
+    Switch,
     Exit,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct AnalyzeParams {
+    code: String,
+    language: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct MethodOnlyParams {
+    mode: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct SwitchParams {
+    code: String,
+    language: String,
+    cursor: Cursor,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct ExitParams;
+
+#[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct CommandParams {
-    pub(crate) code: String,
-    pub(crate) language: String,
-    pub(crate) cursor: crate::core::Cursor,
+    pub(crate) params: serde_json::Value,
+}
+impl CommandParams {
+    pub(crate) fn to_analyze_data(self) -> Result<String, serde_json::Error> {
+        serde_json::from_value(self.params)
+    }
+
+    pub(crate) fn to_method_only_data(self) -> Result<String, serde_json::Error> {
+        serde_json::from_value(self.params)
+    }
+
+    pub(crate) fn to_switch_data(self) -> Result<String, serde_json::Error> {
+        serde_json::from_value(self.params)
+    }
+
+    pub(crate) fn to_exit_data(self) -> Result<String, serde_json::Error> {
+        serde_json::from_value(self.params)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
