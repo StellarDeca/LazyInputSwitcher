6287334ed69c8e0c18b37f8d9b6041b5c8333692
StellarDeca <shiwanshuixing@gmail.com>
2025-12-22 19:01:15 +0800

    Update linux fctix5 query.sh and switch.sh grammar mistake

--- src/static/LinuxMethodShell/fcitx5/switch.sh
+++ src/static/LinuxMethodShell/fcitx5/switch.sh
@@ -2,7 +2,8 @@
 
 # 设置fcitx5为活动状态并切换当前活动输入法到指定输入法
 TARGET_IM="$1"
-STATUS=$(fcitx5-remote &> /dev/null)
+fcitx5-remote &> /dev/null
+STATUS=$?
 
 if [ "$STATUS" -eq 1 ]; then
     fcitx5-remote -o &> /dev/null  # 激活fcitx5
