6ef7d3f8ebfbbd90825056f401ad7a3fe0125f38
StellarDeca <166920291+StellarDeca@users.noreply.github.com>
2026-01-28 19:07:37 +0800

    Fix python comment query grammar error

--- src/static/TreeSitterQuery/python.scm
+++ src/static/TreeSitterQuery/python.scm
@@ -1,5 +1,24 @@
 ; python 注释匹配规则
 (comment) @comment
-module . (expression_statement (string)) @comment
-function_definition (string) @comment
-class_definition (string) @comment
+; 文档开头的多行字符串 (Module Docstring)
+(module
+  . (expression_statement
+      [(string) @comment
+       (concatenated_string) @comment]
+      (#match? @comment "^(\"\"\"|''')")))
+
+; 函数定义后的多行文档字符串
+(function_definition
+  body: (block
+    . (expression_statement
+        [(string) @comment
+         (concatenated_string) @comment]
+        (#match? @comment "^(\"\"\"|''')"))))
+
+; 类定义后的多行文档字符串
+(class_definition
+  body: (block
+    . (expression_statement
+        [(string) @comment
+         (concatenated_string) @comment]
+        (#match? @comment "^(\"\"\"|''')"))))
